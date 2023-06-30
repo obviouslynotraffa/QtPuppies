ea50800016ed5f6fa27937997d4334072cca01a9
raffaele <raffaele.fontana.02@gmail.com>
2023-06-30 23:59:39 +0200

    fix price

--- src/sizes/size.cpp
+++ src/sizes/size.cpp
@@ -1,8 +1,8 @@
 #include "size.h"
 
-unsigned int Size::priceBath=20;
+unsigned int Size::priceBath=10;
 unsigned int Size::priceDiet=15;
-unsigned int Size::priceTraining=50;
+unsigned int Size::priceTraining=35;
 unsigned int Size::priceWalking=10;
 unsigned int Size::priceStay=20;
 
