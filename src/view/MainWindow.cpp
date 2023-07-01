d408b2c956fc02509a7ccf41c0afcfdd33ad233b
raffaele <raffaele.fontana.02@gmail.com>
2023-07-01 16:27:00 +0200

    add status bar

--- src/view/MainWindow.cpp
+++ src/view/MainWindow.cpp
@@ -62,74 +62,6 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow{parent}{
 
     options->addAction(toolb);
 
-
-
-    //set up container
-
-    /*Large* large= new Large();
-    Medium* medium= new Medium();
-    Small* small= new Small();
-
-    //AmStaff* amsatff= new AmStaff();
-    //Bulldog* bulldog= new Bulldog();
-
-    Owner* ow1= new Owner("Pino", "Daniele", "+39 65165132", 18, 5, 1999, "Piazza Garibaldi", "18");
-    Owner* ow2= new Owner("Gennaro", "Bullo", "+39 3698547215", 11, 2, 1984, "Via Falcone Borsellino", "2");
-    Owner* ow3= new Owner("Annalisa", "Di Maggio", "+39 3365214895", 31, 1, 1990, "Via XV Luglio", "30");
-    Owner* ow4= new Owner("Pino", "Daniele", "+39 65165132", 1, 7, 1996, "Via Brombeis", "1");
-
-    owners.push_back(ow1);
-    owners.push_back(ow2);
-    owners.push_back(ow3);
-    owners.push_back(ow4);
-
-
-    Boarding* guest1= new Boarding(20,4,2023,"Jack",medium,ow1,"Boxer",true, false, true, false);
-    Boarding* guest2= new Boarding(20,4,2023,"Pablo",small,ow2,"Pinchir",false, true, false, true);
-    Boarding* guest3= new Boarding(20,4,2023,"Zoe",large,ow3,"Cane-lupo cecoslovacco",false, false, false, false);
-    Boarding* guest4= new Boarding(20,4,2023,"Maya",small,ow4,"Bassotto",true, true, false, true);
-
-
-    Breeding* BulFather= new Breeding(19,11,2015, "Pepe", bulldog,true, false, false);
-    Breeding* BulMother= new Breeding(5,8,2016, "Vik", bulldog,true, false, false);
-
-    Breeding* Bulpuppie1= new Breeding(20,1,2023, "Bink", bulldog, true, false, true, BulMother, BulFather);
-    Breeding* Bulpuppie2= new Breeding(20,1,2023, "Sip", bulldog, true, false, true, BulMother, BulFather);
-    Breeding* Bulpuppie3= new Breeding(20,1,2023, "Zip", bulldog, true, true, true, BulMother, BulFather);
-    Breeding* Bulpuppie4= new Breeding(20,1,2023, "Ettore", bulldog, true, false, true, BulMother, BulFather);
-
-
-    Breeding* AmSFather= new Breeding(19,11,2015, "Orio",amsatff, true, false, false);
-    Breeding* AmSMother= new Breeding(5,8,2016, "Nida", amsatff, true, false, false);
-
-    Breeding* AmSpuppie1= new Breeding(13,8,2022, "Zil", amsatff, true, true, true ,AmSMother, AmSFather);
-    Breeding* AmSpuppie2= new Breeding(13,8,2022, "Paky",amsatff, true, false, true,AmSMother, AmSFather);
-    Breeding* AmSpuppie3= new Breeding(13,8,2022, "Chuck", amsatff, true, true, true, AmSMother, AmSFather);
-    Breeding* AmSpuppie4= new Breeding(13,8,2022, "Bone", amsatff, true, false, true,AmSMother, AmSFather);
-
-
-    c.push_back(BulFather);
-    c.push_back(BulMother);
-    c.push_back(AmSFather);
-    c.push_back(AmSMother);
-
-    c.push_back(AmSpuppie1);
-    c.push_back(AmSpuppie2);
-    c.push_back(AmSpuppie3);
-    c.push_back(AmSpuppie4);
-
-    c.push_back(Bulpuppie1);
-    c.push_back(Bulpuppie2);
-    c.push_back(Bulpuppie3);
-    c.push_back(Bulpuppie4);
-
-
-    c.push_back(guest1);
-    c.push_back(guest2);
-    c.push_back(guest3);
-    c.push_back(guest4);*/
-
-
     //Toolbar
     toolbar = addToolBar("View Toolbar");
     toolbar->addAction(newfile);
@@ -153,6 +85,11 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow{parent}{
     tab_widget->addTab(breeding,"Breeding");
     tab_widget->addTab(boarding,"Boarding");
     setCentralWidget(tab_widget);
+
+    //status= new QStatusBar();
+
+    QMainWindow::statusBar()->showMessage("Ready",3000);
+
 
 
 
@@ -343,6 +280,7 @@ void MainWindow::insertBoarding(Boarding* boardingDog){
 
     general->setContainer(c);
     boarding->setContainer(c.filterBoarding());
+
 
 
 }
@@ -590,6 +528,8 @@ void MainWindow::newDataset(){
     save->setEnabled(true);
     saveAs->setEnabled(true);
 
+    QMainWindow::statusBar()->showMessage("New dataset created",3000);
+
 }
 
 
@@ -623,6 +563,8 @@ void MainWindow::openDataset(){
     save->setEnabled(true);
     saveAs->setEnabled(true);
 
+
+   QMainWindow::statusBar()->showMessage("Dataset opened correctly",3000);
 }
 
 
@@ -631,6 +573,7 @@ void MainWindow::saveDataset(){
     if(repository==nullptr)return;
 
     repository->store();
+    QMainWindow::statusBar()->showMessage("Dataset saved",3000);
 
 }
 
@@ -647,5 +590,6 @@ void MainWindow::saveAsDataset(){
         }
 
         repository->setPath(path.toStdString()).store();
+        QMainWindow::statusBar()->showMessage("Dataset saved as\"" + path + "\".",3000);
 }
 
